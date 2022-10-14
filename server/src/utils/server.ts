ca55c07bd32a2e4abcee9e95cb84341bebdf34a7
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-10-14 01:20:05 -0300

    Importing the routes into the server

--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 
+import routes from '../routes'
+
 function createServer() {
   const app = express()
 
   app.use(express.json())
+
+  routes(app)
   
   return app
 }
